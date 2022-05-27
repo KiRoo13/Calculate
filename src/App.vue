7323149536761b87e77c8256eb7a5a8eae29ae06
Kirill_13 <k1rillrodionov@yandex.ru>
2022-05-27 19:36:55 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <CalculateIn></CalculateIn>
+</template>
+
+<script>
+
+import CalculateIn from './components/CalculateIn.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CalculateIn
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
